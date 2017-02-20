d091e6775f40f91ddebf566fdd21641b93ad4205
Ben Lau <xbenlau@gmail.com>
2017-02-20 12:19:52 +0800

    Revert "Remove all the Java source files"
    This reverts commit aff0ab6809d94c0685f60337a314bc455bdf0289.

---
+++ java/src/androidnative/AndroidNativeActivity.java
@@ -0,0 +1,23 @@
+package androidnative;
+import android.content.Intent;
+
+/** An alternative Activity class for Qt applicaiton.
+
+  Remarks: It is recommended but not a must to use this class as the main activity.
+ */
+
+public class AndroidNativeActivity extends org.qtproject.qt5.android.bindings.QtActivity {
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        SystemDispatcher.onActivityResult(requestCode,resultCode,data);
+    }
+
+    protected void onResume() {
+        super.onResume();
+        SystemDispatcher.onActivityResume();
+    }
+
+}
+
