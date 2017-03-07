4c9ad06a099ff5d174e5810f3c49b440cb57066e
Ben Lau <xbenlau@gmail.com>
2017-03-08 04:03:59 +0800

    Add ExampleActivity.java

---
+++ examples/androidnativeexample/android-sources/src/androidnative/example/ExampleActivity.java
@@ -0,0 +1,16 @@
+package androidnative.example;
+
+import androidnative.AndroidNativeActivity;
+
+/**
+ * Created by benlau on 8/3/2017.
+ */
+
+public class ExampleActivity extends AndroidNativeActivity {
+    public ExampleActivity() {
+        super();
+
+        QT_ANDROID_THEMES = new String[] {""};
+        QT_ANDROID_DEFAULT_THEME = "";
+    }
+}
